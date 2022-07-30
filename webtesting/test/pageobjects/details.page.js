deb54dc6d79ae4d44bf3c24624851aa9d8fcc927
luisaferco <luisa.colorado.e@gmail.com>
2022-07-30 18:22:20 -0500

    add new locators and addItem method

--- webtesting/test/pageobjects/details.page.js
+++ webtesting/test/pageobjects/details.page.js
@@ -4,4 +4,26 @@ class DetailsPage extends Page {
     get addToCartBtn () {
         return $('#productProperties > div.fixedBtn > button');
     }
+
+    get items() {
+        return $$('a.productName');
+    }
+
+    get addToCartBtn() {
+        return $('#productProperties button');
+    }
+
+    get modalTitle()  {
+        return $('#Description > h1');
+    }
+
+    async addItemToCart(itemName){
+        await this.items.find(item => item.getText().includes(itemName)).click();
+        await this.addToCartBtn.click();
+    }
+
+    async getDetailsModal() {
+        return await this.modalTitle.getText();
+    }
+
 }
